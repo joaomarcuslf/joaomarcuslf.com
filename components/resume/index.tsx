0df9aad275fa6ff90b82bdb3be141dd1acaf5c3e
João Marcus de Lemos Fernandes <jmarcusfernandes@gmail.com>
2025-02-20 13:15:30 -0300

    Remove postcss

--- components/resume/index.tsx
+++ components/resume/index.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { usePDF } from "react-to-pdf";
-
 import ContactInfo from "@/components/resume/contact-info";
 import CoreSkillsInfo from "@/components/resume/core-skills-info";
 import DescriptionLines from "@/components/resume/description-line";
@@ -14,26 +12,9 @@ import { JobMetadataWithContent } from "@/types/job";
 
 
 export default function Resume({ topJobs }: { topJobs: JobMetadataWithContent[] }) {
-  const { toPDF, targetRef } = usePDF({
-    filename: `Resume - ${site.author} ${new Date().toISOString()}.pdf`,
-    method: "open",
-    resolution: 1,
-  });
-
-  const hasShowExportPDF = typeof window !== "undefined" && window.location.search.includes("showExportPDF");
-
   return (
     <main>
-      {hasShowExportPDF && <div className="has-text-centered">
-        <button
-          onClick={() => toPDF()}
-          className="button has-text is-dark is-rounded is-outlined"
-        >
-          Export PDF
-        </button>
-      </div>}
-
-      <div ref={targetRef} className="resume">
+      <div className="resume">
         <div className="columns">
           <main className="column is-8">
             <h1>{site.author}</h1>
